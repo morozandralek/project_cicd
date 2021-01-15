200a927709906ddfcf7825d96066306a598bb3d4
Andrey Moroz <morozandralek@gmail.com>
2021-01-15 11:49:42 +0000

    Test

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,17 +12,13 @@ pipeline {
 	                   """
 	          	    }
 
-                    sh """
-                    mkdir fixicch2_dist && cp docker-image/local.app.properties fixicch2_dist
-                    """
-
                      withCredentials([usernamePassword(credentialsId: '12e01b37-86a4-4082-89c0-f1fd4a666769', usernameVariable: 'PSQL_USERNAME', passwordVariable: 'PSQL_PASSWORD')]) {			
 	                   sh """
 	                   ./update_credentials_fixicch2_test.sh ${PSQL_USERNAME} ${PSQL_PASSWORD}
                        """
 	          	    }
 
-                    // sh """ pwd && echo docker-image/local.app.properties"""
+                    sh """cat docker-image/local.app.properties"""
 
             }
         }
--- update_credentials_fixicch2_test.sh
+++ update_credentials_fixicch2_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed  -i -e 's/\(^cuba.dbmsType = \).*/\1postgres/' fixicch2_dist/local.app.properties
-sed  -i -e 's/\(^cuba.dataSource.host = \).*/\110.68.21.181/' fixicch2_dist/local.app.properties
-sed  -i -e 's/\(^cuba.dataSource.port = \).*/\15432/' fixicch2_dist/local.app.properties
-sed  -i -e 's/\(^cuba.dataSource.dbName = \).*/\1fixicch2_test/' fixicch2_dist/local.app.properties
-sed  -i -e 's/\(^cuba.dataSource.username = \).*/\1'$1'/' fixicch2_dist/local.app.properties
-sed  -i -e 's/\(^cuba.dataSource.password = \).*/\1'$2'/' fixicch2_dist/local.app.properties
+sed  -i -e 's/\(^cuba.dbmsType = \).*/\1postgres/' docker-image/local.app.properties
+sed  -i -e 's/\(^cuba.dataSource.host = \).*/\110.68.21.181/' docker-image/local.app.properties
+sed  -i -e 's/\(^cuba.dataSource.port = \).*/\15432/' docker-image/local.app.properties
+sed  -i -e 's/\(^cuba.dataSource.dbName = \).*/\1fixicch2_test/' docker-image/local.app.properties
+sed  -i -e 's/\(^cuba.dataSource.username = \).*/\1'$1'/' docker-image/local.app.properties
+sed  -i -e 's/\(^cuba.dataSource.password = \).*/\1'$2'/' docker-image/local.app.properties
